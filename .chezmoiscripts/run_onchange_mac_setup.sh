25fc0f37588ca7e0657aff3f2da96ef4c957afe6
Varun Datt <35286616+varun-datt@users.noreply.github.com>
2023-07-20 12:13:23 -0500

    mac keyboard settings

--- .chezmoiscripts/run_onchange_mac_setup.sh
+++ .chezmoiscripts/run_onchange_mac_setup.sh
@@ -9,7 +9,8 @@ killall Finder
 
 # Global
 defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
-defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Menu
 defaults write com.apple.menuextra.clock "DateFormat" -string "\"EEE d MMM HH:mm:ss\"" 
