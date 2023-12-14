1975fdd3c6799100356083700948b71e73d83d24
Varun Datt <35286616+varun-datt@users.noreply.github.com>
2023-12-13 19:32:27 -0600

    scripts

--- .chezmoiscripts/run_onchange_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# load fish history file
-
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-
-fisher update
