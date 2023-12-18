ac85cd2f0ec029753ae2ad0d3ccbadc991e95533
Varun Datt <35286616+varun-datt@users.noreply.github.com>
2023-12-18 14:33:06 -0600

    az bastion vm

--- dotfiles/dot_config/private_fish/functions/azBastionVm.fish
+++ dotfiles/dot_config/private_fish/functions/azBastionVm.fish
@@ -1,9 +1,20 @@
-function azBastionVm --description 'start azure vm and bastion tunnel'
-  set -l _vmArgs "--resource-group rg-samurailab-eaus-25 --name vmdevc250 --subscription fce8bb4e-480d-48c7-a9bb-ee2a861dabe2"
+function azBastionVm --description 'start azure vm and bastion tunnel ports'
+  set -l _vmName $argv[1]
+  set -l _resourceGroup $argv[2]
+  set -l _subscription $argv[3]
+
+  set -l _vmArgs "--resource-group $_resourceGroup --name $_vmName --subscription $_subscription"
   set -l _powerState (eval "az vm get-instance-view $_vmArgs | jq -r '.instanceView.statuses[1].displayStatus'")
   if test $_powerState = 'VM deallocated'
     eval "az vm start $_vmArgs"
   end
 
-  az network bastion tunnel --name "bas-samurailab-eaus-public-25" --resource-group "rg-samurailab-eaus-25" --target-resource-id "/subscriptions/fce8bb4e-480d-48c7-a9bb-ee2a861dabe2/resourceGroups/rg-samurailab-eaus-25/providers/Microsoft.Compute/virtualMachines/vmdevc250"  --resource-port 3389 --port 49160 $argv; 
+  set -l _bastionName (az network bastion list --resource-group $_resourceGroup --subscription $_subscription | jq -r 'last | .name')
+
+  az network bastion tunnel \
+    --name $_bastionName \
+    --resource-group $_resourceGroup \
+    --target-resource-id "/subscriptions/$_subscription/resourceGroups/$_resourceGroup/providers/Microsoft.Compute/virtualMachines/$_vmName" \
+    --resource-port 3389 \
+    --port 49160;
 end
