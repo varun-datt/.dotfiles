638a04d50314cbb9aae84b5edef8d458c793a7f3
Varun Datt <35286616+varun-datt@users.noreply.github.com>
2024-03-07 13:21:36 -0600

    windows updates

--- dotfiles/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ dotfiles/Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 # Alias
 Set-Alias -Name g -Value git
 Set-Alias -Name kc -Value kubectl
+Set-Alias -Name kns -Value kubens
 Set-Alias -Name v -Value nvim
 
 # Functions
@@ -18,3 +19,13 @@ Invoke-Expression (& {
 })
 
 Invoke-Expression (&starship init powershell)
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
